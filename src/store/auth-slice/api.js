a258fc5f65e05e78d30015b830ed0948d6718079
Adaeze Ugwumba <144529143+Angee-web@users.noreply.github.com>
2024-12-26 10:42:15 +0100

    Update api.js

--- src/store/auth-slice/api.js
+++ src/store/auth-slice/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-const API_BASE_URL="https://server-xbzz.onrender.com"
+// const API_BASE_URL="https://server-xbzz.onrender.com"
 
 // const API_BASE_URL = "http://localhost:5000";
 
